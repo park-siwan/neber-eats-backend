46de6fd4e861e3092824b302c8871c0f1c39c641
park-siwan <siwandevelop@gmail.com>
2023-09-10 20:58:14 +0900

    feat: resolver 활용해 ts로 graphQL 자동생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
+
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: true,
     }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
