45361062007a3dc984bfbbb9745b86e795a7bca5
park-siwan <siwandevelop@gmail.com>
2023-09-11 11:34:15 +0900

    feat: InputTypes and ArgumentTypes

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -4,6 +4,13 @@ import { Field, ObjectType } from '@nestjs/graphql';
 export class Restaurant {
   @Field(() => String)
   name: string;
+
   @Field(() => Boolean, { nullable: true })
-  isGood: boolean;
+  isVegan: boolean;
+
+  @Field((type) => String)
+  address: string;
+
+  @Field((type) => String)
+  ownerName: string;
 }
