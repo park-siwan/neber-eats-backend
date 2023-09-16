93d5790d372057445e0f5353b0522d30ec783001
park-siwan <siwandevelop@gmail.com>
2023-09-16 16:25:46 +0900

    5.6 An Alternative Error

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,6 +6,11 @@ import {
   CreateAccountOutput,
 } from './dtos/create-account.dto';
 
+/** resolver가 하는 일은 오직 input 가지고 output을 보내는 것
+ * ex) if else 사용은 여기서 안함
+ *  -> 로직은 service.ts 에서 하기
+ */
+
 @Resolver((of) => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
@@ -20,16 +25,10 @@ export class UserResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const error = await this.userService.createAccount(createAccountInput);
-      if (error) {
-        return {
-          ok: false,
-          error,
-        };
-      }
-      return {
-        ok: true,
-      };
+      const { ok, error } = await this.userService.createAccount(
+        createAccountInput,
+      );
+      return { ok, error };
     } catch (error) {
       return { error, ok: false };
     }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,17 +14,17 @@ export class UserService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<string | undefined> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exist = await this.users.findOne({ where: { email } });
       if (exist) {
-        //make error
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
       //make error
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
