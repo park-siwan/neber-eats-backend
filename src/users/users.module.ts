cdefd169dd8ce7c34513adf9f1c8f487fe0ad68d
park-siwan <siwandevelop@gmail.com>
2023-09-20 18:52:51 +0900

    fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,7 +6,7 @@ import { UserService } from './users.service';
 import { Verification } from './entities/verification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), Verification],
+  imports: [TypeOrmModule.forFeature([User, Verification])],
   providers: [UserResolver, UserService],
   exports: [UserService],
 })
