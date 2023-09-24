e373bf0fa909240cec3c60cc5322e5b71f688f33
park-siwan <siwandevelop@gmail.com>
2023-09-24 16:07:25 +0900

    7.6 Mail Module Setup

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,22 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Module({
+  providers: [],
+})
+@Global()
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    return {
+      module: MailModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+      ],
+      exports: [],
+    };
+  }
+}
