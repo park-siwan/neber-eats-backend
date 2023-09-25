92af36259bf50c7d097d30a4ef8904b49803ae1c
park-siwan <siwandevelop@gmail.com>
2023-09-25 14:48:07 +0900

    7.9 mail refactor

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { MailModuleOptions } from './mail.interfaces';
+import { EmailVar, MailModuleOptions } from './mail.interfaces';
 import * as FormData from 'form-data';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import got from 'got';
@@ -11,27 +11,41 @@ export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {
-    this.sendEmail('testing', 'test');
+    // this.sendEmail('testing', 'test');
   }
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(
+    subject: string,
+    template: string,
+    emailVars: EmailVar[],
+  ) {
     const mailgun = new Mailgun(FormData);
     const mg = mailgun.client({
       username: 'api',
       key: this.options.apiKey,
     });
 
-    mg.messages
-      .create(this.options.domain, {
-        from: `Excited User <mailgun@${this.options.domain}>`,
-        to: ['siwandevelop@gmail.com'],
-        subject: 'Hello2',
-        template: 'confirm mail',
-        ['X-Mailgun-Variables']: '{"test": "test"}',
-        ['v:code']: 'asdfsd',
-        ['v:username']: 'siwan!!!',
-      })
-      .then((msg) => console.log(msg)) // logs response data
-      .catch((err) => console.log(err)); // logs any error
+    const createObj = {
+      from: `Siwan from Nuber Eats <mailgun@${this.options.domain}>`,
+      to: ['siwandevelop@gmail.com'],
+      subject: 'Hello2',
+      template: template,
+      ['X-Mailgun-Variables']: '{"test": "test"}',
+    };
+    emailVars.forEach(({ key, value }) => {
+      createObj['v:' + key] = value;
+    });
+
+    try {
+      mg.messages.create(this.options.domain, createObj);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  sendVerificationEmail(email: string, code: string) {
+    this.sendEmail('Verify Your Email', 'confirm mail', [
+      { key: 'code', value: code },
+      { key: 'username', value: email },
+    ]);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,6 +12,7 @@ import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { Verification } from './entities/verification.entity';
 import { UserProfileOutput } from './dtos/user-profile.dto';
 import { VerifyEmailOutput } from 'src/common/dtos/verify-email.dto';
+import { MailService } from 'src/mail/mail.service';
 @Injectable()
 export class UserService {
   constructor(
@@ -19,6 +20,7 @@ export class UserService {
     @InjectRepository(Verification)
     private readonly verifications: Repository<Verification>,
     private readonly jwtService: JwtService,
+    private readonly mailService: MailService,
   ) {}
 
   async createAccount({
@@ -34,7 +36,10 @@ export class UserService {
       const user = await this.users.save(
         this.users.create({ email, password, role }),
       );
-      await this.verifications.save(this.verifications.create({ user }));
+      const verification = await this.verifications.save(
+        this.verifications.create({ user }),
+      );
+      this.mailService.sendVerificationEmail(user.email, verification.code);
       return { ok: true };
     } catch (e) {
       //make error
@@ -96,7 +101,11 @@ export class UserService {
       if (email) {
         user.email = email;
         user.verified = false;
-        await this.verifications.save(this.verifications.create({ user }));
+        await this.verifications.delete({ user: { id: user.id } });
+        const verification = await this.verifications.save(
+          this.verifications.create({ user }),
+        );
+        this.mailService.sendVerificationEmail(user.email, verification.code);
       }
       if (password) {
         user.password = password;
