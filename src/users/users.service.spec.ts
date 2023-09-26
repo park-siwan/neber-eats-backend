7be90745180d541d0c7c4caebd9fc6e8bc026b09
park-siwan <siwandevelop@gmail.com>
2023-09-26 19:22:09 +0900

    8.12 verifyEmail Test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -13,6 +13,7 @@ const mockRepository = () => ({
   save: jest.fn(),
   create: jest.fn(),
   findOneOrFail: jest.fn(),
+  delete: jest.fn(),
 });
 
 const mockJwtService = () => ({
@@ -249,7 +250,43 @@ describe('UserService', () => {
       const result = await service.editProfile(1, { email: '12' });
       expect(result).toEqual({ ok: false, error: 'Could not update profile' });
     });
-  });
 
-  it.todo('verifyEmail');
+    describe('verifyEmail', () => {
+      it('should verify email', async () => {
+        const mockedVerification = {
+          user: {
+            verified: false,
+          },
+          id: 1,
+        };
+        verificationsRepository.findOne.mockResolvedValue(mockedVerification);
+
+        const result = await service.verifyEmail('');
+
+        expect(verificationsRepository.findOne).toHaveBeenCalledTimes(1);
+        expect(verificationsRepository.findOne).toHaveBeenCalledWith(
+          expect.any(Object),
+        );
+        expect(usersRepository.save).toHaveBeenCalledTimes(1);
+        expect(usersRepository.save).toHaveBeenCalledWith({ verified: true });
+
+        expect(verificationsRepository.delete).toHaveBeenCalledTimes(1);
+        expect(verificationsRepository.delete).toHaveBeenCalledWith(
+          mockedVerification.id,
+        );
+        expect(result).toEqual({ ok: true });
+      });
+
+      it('should fail on verification not found', async () => {
+        const result = await service.verifyEmail('');
+        expect(result).toEqual({ ok: false, error: 'Verification no found' });
+      });
+
+      it('should fail on exception', async () => {
+        verificationsRepository.findOne.mockRejectedValue(new Error());
+        const result = await service.verifyEmail('');
+        expect(result).toEqual({ ok: false, error: 'Could not verify email.' });
+      });
+    });
+  });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -117,9 +117,9 @@ export class UserService {
         await this.verifications.delete(verification.id);
         return { ok: true };
       }
-      throw new Error();
+      return { ok: false, error: 'Verification not found.' };
     } catch (error) {
-      return { ok: false, error };
+      return { ok: false, error: 'Could not verify email.' };
     }
   }
 }
