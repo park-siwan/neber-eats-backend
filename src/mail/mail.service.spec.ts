5ac1a83089a0743b71bd3b19f5a3c4c43281eaf6
park-siwan <siwandevelop@gmail.com>
2023-09-27 12:28:59 +0900

    9.2 MailService

---
+++ src/mail/mail.service.spec.ts
@@ -0,0 +1,35 @@
+import { Test } from '@nestjs/testing';
+import got from 'got';
+import * as FormData from 'form-data';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailService } from './mail.service';
+
+jest.mock('got');
+jest.mock('form-data');
+
+const TEST_DOMAIN = 'test-domain';
+
+describe('MailService', () => {
+  let service: MailService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        MailService,
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: {
+            apiKey: 'test-apiKey',
+            domain: TEST_DOMAIN,
+            fromEmail: 'test-fromEmail',
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<MailService>(MailService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
