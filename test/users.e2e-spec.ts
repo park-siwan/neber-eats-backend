04d3ff602a6452b9d348ead283dd44dc975def0f
park-siwan <siwandevelop@gmail.com>
2023-09-29 22:41:00 +0900

    10.2 testing createAccount part One

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
-
+const GRAPHQL_ENDPOINT = '/graphql';
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -31,7 +31,33 @@ describe('UserModule (e2e)', () => {
     await app.close();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const EMAIL = 'siwan@gmail.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"12345",
+            role:Owner
+          }){
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+    it.todo('should fail if account already exists');
+  });
   it.todo('userProfile');
   it.todo('login');
   it.todo('me');
