ea24cb560e39d69a8945f6a97d340f0bd52b4685
park-siwan <siwandevelop@gmail.com>
2023-10-01 11:58:09 +0900

    10.4 Testing login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { DataSource } from 'typeorm';
-const GRAPHQL_ENDPOINT = '/graphql';
+import { DataSource, Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 jest.mock('got', () => {
   return {
@@ -11,15 +12,26 @@ jest.mock('got', () => {
   };
 });
 
+const GRAPHQL_ENDPOINT = '/graphql';
+
+const testUser = {
+  email: 'nico@las.com',
+  password: '12345',
+};
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  // let usersRepository: Repository<User>;
+  let jwtToken: string;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
+    // usersRepository = module.get<Repository<User>>(getRepositoryToken(User));
+
     await app.init();
   });
 
@@ -47,8 +59,8 @@ describe('UserModule (e2e)', () => {
           query: `
         mutation {
           createAccount(input:{
-            email:"${EMAIL}",
-            password:"12345",
+            email:"${testUser.email}",
+            password:"${testUser.password}",
             role:Owner
           }){
             ok
@@ -70,8 +82,8 @@ describe('UserModule (e2e)', () => {
           query: `
         mutation {
           createAccount(input: {
-            email:"${EMAIL}",
-            password:"12345",
+            email:"${testUser.email}",
+            password:"${testUser.password}",
             role:Owner
           }) {
             ok
@@ -89,8 +101,69 @@ describe('UserModule (e2e)', () => {
         });
     });
   });
+  describe('login', () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+      mutation {
+        login(input: {
+          email:"${testUser.email}",
+          password:"${testUser.password}",
+        }) {
+          ok
+          error
+          token
+        }
+      }
+      `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        });
+    });
+    it('should not be able login with wrong credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+      mutation {
+        login(input: {
+          email:"${testUser.email}",
+          password: "xxx",
+        }) {
+          ok
+          error
+          token
+        }
+      }
+      `,
+        })
+        .expect(200)
+        .expect((res) => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toEqual(null);
+          jwtToken = login.token;
+        });
+    });
+  });
   it.todo('userProfile');
-  it.todo('login');
   it.todo('me');
   it.todo('verifyEmail');
   it.todo('editProfile');
