4b76e9db30260d832ba69b5d0d494eb40cc8b349
park-siwan <siwandevelop@gmail.com>
2023-10-18 17:00:44 +0900

    이렇게 해야 https://studio.apollographql.com/sandbox/explorer 에서 확인가능

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // CORS 미들웨어를 사용하여 CORS 설정 적용
+  app.enableCors({
+    origin: 'https://studio.apollographql.com', // 허용할 출처
+    methods: 'GET', // 허용할 HTTP 메서드
+    allowedHeaders: 'Content-Type, Accept', // 허용할 헤더
+  });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
