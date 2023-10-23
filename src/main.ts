f9b9d2b374f28c6459fb29849c89ae0767fea78f
park-siwan <siwandevelop@gmail.com>
2023-10-23 16:00:55 +0900

    fix: allowHeaders에 허용할 헤더 추가

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   // CORS 미들웨어를 사용하여 CORS 설정 적용
   app.enableCors({
-    origin: 'https://studio.apollographql.com', // 허용할 출처
-    methods: 'GET', // 허용할 HTTP 메서드
-    allowedHeaders: 'Content-Type, Accept', // 허용할 헤더
+    origin: ['https://studio.apollographql.com', 'http://localhost:4001'], // 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    allowedHeaders: ['Content-Type', 'Accept', 'Origin'], // 허용할 헤더
   });
   await app.listen(process.env.PORT || 4000);
 }
